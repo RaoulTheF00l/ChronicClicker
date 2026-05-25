e88d32c2804c2e68636434b35e6f33667a33d18d
RaoulTheF00l <conore21@gmail.com>
2026-05-24 22:20:35 -0400

    Title to gameplay transition added

--- Scripts/title.gd
+++ Scripts/title.gd
@@ -1,19 +1,15 @@
 extends Control
 
-@onready var title_label: Label = $Layout/TitleLabel
-@onready var play_button: Button = $Layout/PlayButton
-@onready var quit_button: Button = $Layout/QuitButton
-@onready var layout: VBoxContainer = $Layout
+
+@onready var Play_Button : Button = $Layout/PlayButton
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_on_play_button_pressed()
+	pass # Replace with function body.
+
+
 
 
 func _on_play_button_pressed() -> void:
-00	get_tree().change_scene_to_file("res://Scenes/main.tscn")
-
-
-func _on_quit_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
